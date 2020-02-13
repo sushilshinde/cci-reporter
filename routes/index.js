72ef68815013afa08bfe825ededc5a82ed8d3f6d
Sushil Shinde <shinde.sushil@gmail.com>
2020-02-13 16:07:35 +0530

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',x: "Hello" });
+  res.render('index', { title: 'Express',x: "Hello gtom sjkflsdfjlsdf" });
 });
 
 module.exports = router;
